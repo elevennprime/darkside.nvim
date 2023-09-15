0a774d5b14dc889003ee7c5773c6bf9e28b6068e
Adam Elevenn <adam@elevenn.me>
2023-09-15 14:39:51 +0100

    style(lsp): add some lsp nightlight groups

--- lua/darkside/group/modules/lsp.lua
+++ lua/darkside/group/modules/lsp.lua
@@ -5,6 +5,12 @@ function M.get(palette, config)
 		LspReferenceText  = {bg = palette.selection, underline = true}, -- used for highlighting "text" references
 		LspReferenceRead  = {link = "LspReferenceText"}, -- used for highlighting "read" references
 		LspReferenceWrite = {link = "LspReferenceText"}, -- used for highlighting "write" references
+		-- Used to color the virtual text of the codelens. See
+		LspCodeLens = {},
+		-- Used to color the separator between two or more code lenses.
+		LspCodeLensSeparator = {},
+		-- Used to highlight the active parameter in the signature help. See vim.lsp.handlers.signature_help()
+		LspSignatureActiveParameter = {fg = palette.fg, bold = true},
 	}
 end
 
