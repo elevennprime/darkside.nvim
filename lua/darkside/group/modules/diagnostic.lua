9dea058b40c6aba93b8848cca29971fd9394fe8c
Adam Elevenn <adam@elevenn.me>
2023-09-17 15:02:12 +0100

    style(diagnostic): change some styling

--- lua/darkside/group/modules/diagnostic.lua
+++ lua/darkside/group/modules/diagnostic.lua
@@ -3,25 +3,44 @@ local M = {}
 function M.get(palette)
 	return {
 		DiagnosticError            = {fg = palette.error},
-		DiagnosticVirtualTextError = {fg = palette.error},
-		DiagnosticFloatingError    = {fg = palette.error},
-		DiagnosticSignError        = {fg = palette.error, bg = palette.bg_sign},
-		DiagnosticUnderlineError   = {undercurl = true, sp = palette.error},
 		DiagnosticWarn             = {fg = palette.yellow},
-		DiagnosticVirtualTextWarn  = {fg = palette.yellow},
-		DiagnosticFloatingWarn     = {fg = palette.yellow},
-		DiagnosticSignWarn         = {fg = palette.yellow, bg = palette.bg_sign},
-		DiagnosticUnderlineWarn    = {undercurl = true, sp = palette.yellow},
 		DiagnosticInformation      = {fg = palette.paleblue},
-		DiagnosticVirtualTextInfo  = {fg = palette.paleblue},
-		DiagnosticFloatingInfo     = {fg = palette.paleblue},
-		DiagnosticSignInfo         = {fg = palette.paleblue, bg = palette.bg_sign},
-		DiagnosticUnderlineInfo    = {undercurl = true, sp = palette.paleblue},
 		DiagnosticHint             = {fg = palette.purple},
+
+		DiagnosticOk               = {},
+
+		DiagnosticVirtualTextError = {fg = palette.error},
+		DiagnosticVirtualTextWarn  = {fg = palette.yellow},
+		DiagnosticVirtualTextInfo  = {fg = palette.paleblue},
 		DiagnosticVirtualTextHint  = {fg = palette.purple},
-		DiagnosticFloatingHint     = {fg = palette.purple},
-		DiagnosticSignHint         = {fg = palette.purple, bg = palette.bg_sign},
+		DiagnosticVirtualTextOk    = {},
+
+		DiagnosticUnderlineError   = {undercurl = true, sp = palette.error},
+		DiagnosticUnderlineWarn    = {undercurl = true, sp = palette.yellow},
+		DiagnosticUnderlineInfo    = {undercurl = true, sp = palette.paleblue},
 		DiagnosticUnderlineHint    = {undercurl = true, sp = palette.purple},
+		DiagnosticUnderlineOk      = {},
+
+		DiagnosticFloatingError    = {fg = palette.error},
+		DiagnosticFloatingWarn     = {fg = palette.yellow},
+		DiagnosticFloatingInfo     = {fg = palette.paleblue},
+		DiagnosticFloatingHint     = {fg = palette.purple},
+		DiagnosticFloatingOk       = {},
+
+
+		-- Used for "Error" signs in sign column.
+		DiagnosticSignError        = {},
+		DiagnosticSignWarn         = {},
+		DiagnosticSignInfo         = {},
+		DiagnosticSignHint         = {},
+		DiagnosticSignOk           = {},
+
+		-- Used for deprecated or obsolete code.
+		DiagnosticDeprecated = {},
+
+		-- Used for unnecessary or unused code.
+		DiagnosticUnnecessary = {link = "Comment"},
+
 	}
 end
 
